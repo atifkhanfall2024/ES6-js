284ebc629c4e4c68a18cf3091b10834fe74ecac9
MUhammad Atif khan <muhammadatifkhan0906@gmail.com>
2025-02-05 21:21:58 +0500

    done

--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
 this topic is totally cover advance java script
+
+
+// let and ocnst both are different 
+let make a varaible and const make a constant 
+
+// let and const add itself on window objects
+
--- index.js
+++ index.js
@@ -5,5 +5,37 @@
 // const a = 12 ; a = 22     not possible 
 
 
-  
+     function myfunc(){
+          alert("button clicked")
+     }
+
+    //console.log("hello ");
+
+
+     // fat arrow function   ()=>{}
+
+// arrow function  // arrow function func with one parms // arrow func with implicit return
+
+
+   var arrow = ()=>{}
+
+   // arrow func with one parms 
+
+   var arrow  = (parm)=>{}
+   arrow(12)
+
+
+   // implicit return 
+
+  var c  =  ()=>12
+  console.log(c());
+
+
+
+     // templete literals backtick
+      
+     // if i want to add someting then we use backtick also and also string below an example
+
+  console.log("hey"+ 2+2+"is 4");
+  console.log(`hey ${2+2} is 4`);
   
