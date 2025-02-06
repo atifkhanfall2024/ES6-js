9017f03ba45b40fa6b3622737cfa576d5732e9ee
MUhammad Atif khan <muhammadatifkhan0906@gmail.com>
2025-02-06 15:52:14 +0500

    promise

---
+++ RestSpread/Async.js
@@ -0,0 +1,10 @@
+// javascript is by default sync by default and also single threded language
+
+
+// blocking vs non blocking code 
+
+// blocking work on sycn and block the flow of code 
+// while non blocking work on async and stop the folw of code 
+
+
+// we cannot say that async is better than sync it depend on code
